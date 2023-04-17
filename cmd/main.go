a518bfd319907e16dbd5ae0e88b3fbfca66f5d29
Aidana <aidana.amnbk@gmail.com>
2023-04-17 17:51:22 +0600

    start

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	pkg "forum-v1/pkg"
+)
+
 func main() {
+	// mux := http.NewServeMux()
+	// mux.Handle("/", home)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", pkg.Home)
+	mux.Handle("/image/", http.StripPrefix("/image", http.FileServer(http.Dir("./web/image"))))
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./web/static"))))
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ pkg/home.go
@@ -0,0 +1,11 @@
+package pkg
+
+import "net/http"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Start"))
+}
